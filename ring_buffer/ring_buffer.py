39cab5c962640032fa9d8f739cc1ee57f4b9a6b9
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2020-06-12 10:58:35 -0500

    ring_buffer.py

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,22 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None] * capacity
+        # pre_allocated_list = [None] * size
+        # to pre-allocate a list (that is, to be able to address
+        # 'size' elements of the list instead of gradually forming
+        # the list by appending). This operation is VERY fast, even on big lists
 
-    def append(self, item):
-        pass
+    def append(self, item):  # When the ring buffer is full and a new element is inserted
+        # self.storage will create the new list self.current
+        self.storage[self.current] = item
+        self.current += 1
+        if self.current == self.capacity:
+            self.current = 0
 
     def get(self):
-        pass
+        if self.storage[-1] is None:
+            # use a slice, With slices, we can call multiple character values
+            return self.storage[:self.current]
+        return self.storage
